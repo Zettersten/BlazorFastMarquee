f8d32f196275024c8a8a6aa3f784813869a0cce4
Erik Zettersten <erik@zettersten.com>
2025-10-01 13:50:24 -0400

    Rename marquee component and streamline interop

--- Components/Marquee.razor.cs
+++ Components/Marquee.razor.cs
@@ -1,49 +1,20 @@
-@using System.Collections.Generic
-@using System.Globalization
-@using System.Linq
-@using System.Text
-@inject IJSRuntime JS
-@implements IAsyncDisposable
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
 
-<div
-    @ref="_containerRef"
-    class="@CssClass"
-    style="@ContainerStyle"
-    @attributes="_attributesWithoutClassOrStyle">
-    @if (Gradient)
-    {
-        <div class="bfm-overlay" style="@GradientStyle"></div>
-    }
-    <div class="bfm-marquee"
-         style="@MarqueeStyle"
-         @onanimationiteration="HandleIteration"
-         @onanimationend="HandleAnimationEnd">
-        <div class="bfm-initial-child-container" @ref="_marqueeRef">
-            <div class="bfm-child" style="@ChildStyle">
-                @ChildContent
-            </div>
-        </div>
-        @for (var i = 0; i < Math.Max(0, _multiplier - 1); i++)
-        {
-            <div class="bfm-child" style="@ChildStyle" @key=$"initial-{i}">
-                @ChildContent
-            </div>
-        }
-    </div>
-    <div class="bfm-marquee" style="@MarqueeStyle">
-        @for (var i = 0; i < _multiplier; i++)
-        {
-            <div class="bfm-child" style="@ChildStyle" @key=$"clone-{i}">
-                @ChildContent
-            </div>
-        }
-    </div>
-</div>
+namespace BlazorFastMarquee;
 
-@code {
-    private static readonly string ModulePath = $"./_content/{typeof(FastMarquee).Assembly.GetName().Name}/js/marquee.js";
+public sealed partial class Marquee : ComponentBase, IAsyncDisposable
+{
+    private static readonly string ModulePath = $"./_content/{typeof(Marquee).Assembly.GetName().Name}/js/marquee.js";
 
-    private readonly DotNetObjectReference<FastMarquee> _dotNetRef;
+    private readonly DotNetObjectReference<Marquee> _dotNetRef;
     private ElementReference _containerRef;
     private ElementReference _marqueeRef;
     private IJSObjectReference? _module;
@@ -52,36 +23,57 @@
     private double _containerSpan;
     private double _marqueeSpan;
     private int _multiplier = 1;
-    private MarqueeOptions _appliedOptions;
     private string? _additionalClass;
     private string? _additionalStyle;
     private IReadOnlyDictionary<string, object>? _attributesWithoutClassOrStyle;
 
-    public FastMarquee()
+    public Marquee()
     {
         _dotNetRef = DotNetObjectReference.Create(this);
-        _appliedOptions = BuildOptions();
     }
 
+    [Inject]
+    private IJSRuntime JS { get; set; } = default!;
+
     [Parameter] public string? ClassName { get; set; } = string.Empty;
+
     [Parameter] public string? Style { get; set; } = string.Empty;
+
     [Parameter] public bool AutoFill { get; set; }
+
     [Parameter] public bool Play { get; set; } = true;
+
     [Parameter] public bool PauseOnHover { get; set; }
+
     [Parameter] public bool PauseOnClick { get; set; }
+
     [Parameter] public MarqueeDirection Direction { get; set; } = MarqueeDirection.Left;
+
     [Parameter] public double Speed { get; set; } = 50d;
+
     [Parameter] public double Delay { get; set; }
+
     [Parameter] public int Loop { get; set; }
+
     [Parameter] public bool Gradient { get; set; }
+
     [Parameter] public string GradientColor { get; set; } = "white";
+
     [Parameter] public CssLength GradientWidth { get; set; } = new CssLength(200);
+
     [Parameter] public EventCallback OnFinish { get; set; }
+
     [Parameter] public EventCallback OnCycleComplete { get; set; }
+
     [Parameter] public EventCallback OnMount { get; set; }
+
     [Parameter] public RenderFragment? ChildContent { get; set; }
+
     [Parameter(CaptureUnmatchedValues = true)]
     public IReadOnlyDictionary<string, object>? AdditionalAttributes { get; set; }
+
+    protected IReadOnlyDictionary<string, object>? AdditionalAttributesWithoutClassOrStyle
+        => _attributesWithoutClassOrStyle;
 
     private string CssClass
         => string.Join(' ', new[] { "bfm-marquee-container", ClassName, _additionalClass }
@@ -150,36 +142,97 @@
 
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
-        if (firstRender)
+        await EnsureModuleAsync();
+
+        if (_module is null)
         {
-            _module = await JS.InvokeAsync<IJSObjectReference>("import", ModulePath);
-            _observer = await _module.InvokeAsync<IJSObjectReference>("initialize", _containerRef, _marqueeRef, BuildOptions(), _dotNetRef);
-            _appliedOptions = BuildOptions();
-            if (OnMount.HasDelegate && !_onMountInvoked)
-            {
-                _onMountInvoked = true;
-                await OnMount.InvokeAsync();
-            }
             return;
         }
 
-        if (_observer is not null)
+        await EnsureObserverAsync();
+        await MeasureAsync();
+
+        if (firstRender && OnMount.HasDelegate && !_onMountInvoked)
         {
-            var options = BuildOptions();
-            if (!_appliedOptions.Equals(options))
-            {
-                _appliedOptions = options;
-                await _observer.InvokeVoidAsync("updateOptions", options);
-            }
-            else
-            {
-                await _observer.InvokeVoidAsync("forceMeasure");
-            }
+            _onMountInvoked = true;
+            await OnMount.InvokeAsync();
         }
     }
 
     [JSInvokable]
     public Task UpdateLayout(double containerSpan, double marqueeSpan)
+    {
+        if (ApplyLayout(containerSpan, marqueeSpan))
+        {
+            return InvokeAsync(StateHasChanged);
+        }
+
+        return Task.CompletedTask;
+    }
+
+    private async Task EnsureModuleAsync()
+    {
+        if (_module is not null)
+        {
+            return;
+        }
+
+        _module = await JS.InvokeAsync<IJSObjectReference>("import", ModulePath);
+    }
+
+    private async Task EnsureObserverAsync()
+    {
+        if (_module is null)
+        {
+            return;
+        }
+
+        var vertical = IsVertical(Direction);
+
+        if (_observer is null)
+        {
+            _observer = await _module.InvokeAsync<IJSObjectReference>("observe", _containerRef, _marqueeRef, vertical, _dotNetRef);
+        }
+        else
+        {
+            await _observer.InvokeVoidAsync("update", vertical);
+        }
+    }
+
+    private async Task DisposeObserverAsync()
+    {
+        if (_observer is null)
+        {
+            return;
+        }
+
+        try
+        {
+            await _observer.InvokeVoidAsync("dispose");
+        }
+        finally
+        {
+            await _observer.DisposeAsync();
+            _observer = null;
+        }
+    }
+
+    private async Task MeasureAsync()
+    {
+        if (_module is null)
+        {
+            return;
+        }
+
+        var vertical = IsVertical(Direction);
+        var measurement = await _module.InvokeAsync<MarqueeMeasurement>("measure", _containerRef, _marqueeRef, vertical);
+        if (ApplyLayout(measurement.ContainerSpan, measurement.MarqueeSpan))
+        {
+            await InvokeAsync(StateHasChanged);
+        }
+    }
+
+    private bool ApplyLayout(double containerSpan, double marqueeSpan)
     {
         var hasChanged = false;
         if (!AreClose(containerSpan, _containerSpan))
@@ -204,12 +257,7 @@
             hasChanged = true;
         }
 
-        if (hasChanged)
-        {
-            return InvokeAsync(StateHasChanged);
-        }
-
-        return Task.CompletedTask;
+        return hasChanged;
     }
 
     private void HandleIteration(AnimationIterationEventArgs args)
@@ -281,6 +329,21 @@
         _attributesWithoutClassOrStyle = filtered;
     }
 
+    public async ValueTask DisposeAsync()
+    {
+        _dotNetRef.Dispose();
+        await DisposeObserverAsync();
+
+        if (_module is not null)
+        {
+            await _module.DisposeAsync();
+            _module = null;
+        }
+    }
+
+    private static bool IsVertical(MarqueeDirection direction)
+        => direction is MarqueeDirection.Up or MarqueeDirection.Down;
+
     private static void AppendCssVariable(ref StringBuilder builder, string name, string value)
     {
         builder.Append("--");
@@ -318,41 +381,5 @@
     private static bool AreClose(double left, double right)
         => Math.Abs(left - right) < 0.1d;
 
-    public async ValueTask DisposeAsync()
-    {
-        _dotNetRef.Dispose();
-        if (_observer is not null)
-        {
-            try
-            {
-                await _observer.InvokeVoidAsync("dispose");
-            }
-            finally
-            {
-                await _observer.DisposeAsync();
-                _observer = null;
-            }
-        }
-
-        if (_module is not null)
-        {
-            await _module.DisposeAsync();
-            _module = null;
-        }
-    }
-
-    private MarqueeOptions BuildOptions()
-        => new(GetDirectionName(Direction), AutoFill);
-
-    private static string GetDirectionName(MarqueeDirection direction)
-        => direction switch
-        {
-            MarqueeDirection.Left => "left",
-            MarqueeDirection.Right => "right",
-            MarqueeDirection.Up => "up",
-            MarqueeDirection.Down => "down",
-            _ => "left"
-        };
-
-    private readonly record struct MarqueeOptions(string Direction, bool AutoFill);
+    private readonly record struct MarqueeMeasurement(double ContainerSpan, double MarqueeSpan);
 }
