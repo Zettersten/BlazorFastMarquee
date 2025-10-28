d53c463a79b487dca62ef46e760a4822d8e79c55
zettersten <erik@veefriends.com>
2025-10-28 11:36:43 -0400

    dupe startup

--- samples/BlazorFastMarquee.Demo/Program.cs
+++ samples/BlazorFastMarquee.Demo/Program.cs
@@ -3,7 +3,10 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorFastMarquee.Demo.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<Routes>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+// Add services if needed
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
